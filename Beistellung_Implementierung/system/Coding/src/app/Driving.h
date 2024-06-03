14d414ae88d95d2ae0c0d018ee576c621b82117e
steffenhettig <145577050+steffenhettig@users.noreply.github.com>
2024-06-03 18:05:41 +0200

    Update Driving.h

--- Beistellung_Implementierung/system/Coding/src/app/Driving.h
+++ Beistellung_Implementierung/system/Coding/src/app/Driving.h
@@ -16,6 +16,7 @@
 #define SENSOR_WEIGHT_SCALE 1000
 #define CENTER_OF_LINE_POSITION 2000
 #define MAX_MOTOR_SPEED 400
+#define CALIB_OVER_LINE(value) ((value) > 500)
 
 /* MACROS *****************************************************************************************/
 
