0cb61e6ec3b7e305a75c86f1ce48f6d73ab48cd9
MaximilianSchleif <163732646+MaximilianSchleif@users.noreply.github.com>
2024-06-05 09:16:26 +0200

    Update ModeSwitch_Handler.h
    Dateifehlel nach Upload behoben

--- Beistellung_Implementierung/system/Coding/src/app/ModeSwitch_Handler.h
+++ Beistellung_Implementierung/system/Coding/src/app/ModeSwitch_Handler.h
@@ -2,27 +2,25 @@
   (c) NewTec GmbH 2024   -   www.newtec.de
 ***************************************************************************************************/
 /**
- * @file       MODESWITCH_HANDLER.h
+ * @file       ModeSwitch.h
  *
- * Module description comes here.
  */
  /**************************************************************************************************/
+
 #ifndef MODESWITCH_HANDLER_H
-#define VMODESWITCH_HANDLER_H
+#define MODESWITCH_HANDLER_H
 
 /* INCLUDES ***************************************************************************************/
+#include "Common/Types.h"
 
 /* CONSTANTS **************************************************************************************/
-
 /* MACROS *****************************************************************************************/
-
 /* TYPES ******************************************************************************************/
+/* PROTOTYPES *************************************************************************************/
 
-// Definition of the ModeSwitch_Handler class
-typedef struct {
-    // Methoden
-    void (*ModeSwitch_SwitchToNextParameterSet)(void);
-} ModeSwitch_Handler;
+void ModeSwitch_process(void);
+
+#endif /* MODESWITCH_HANDLER_H */
 
 /* PROTOTYPES *************************************************************************************/
 
