f6fb195fe6fd6896345cbaee87f4b87c9bf3f230
MaximilianSchleif <163732646+MaximilianSchleif@users.noreply.github.com>
2024-06-05 09:10:57 +0200

    Update ModeSwitch_Handler.c
    Dateifehler beim Upload behoben

--- Beistellung_Implementierung/system/Coding/src/app/ModeSwitch_Handler.c
+++ Beistellung_Implementierung/system/Coding/src/app/ModeSwitch_Handler.c
@@ -1,17 +1,21 @@
 /***************************************************************************************************
+/***************************************************************************************************
   (c) NewTec GmbH 2024   -   www.newtec.de
 ***************************************************************************************************/
 /**
- * @file       Vorlage.c
- *
- * Module description comes here.
+ * @file       ModeSwitch.c
+ * After setting the global variable gModeStatus to status code 0, the ModeSwitch runs 
+ * through a loop in which the button states of buttons A, B, C are queried. 
+ * If the status code changes, the loop is exited and the return to the StateHandler follows.
+ * The global variable is queried in StartRace.c using a switch case and a different mode is executed accordingly.
  */
  /**************************************************************************************************/
 
  /* INCLUDES ***************************************************************************************/
 #include <stdio.h>
-#include "ModeSwitch_Handler.h"
-#include "Button.h"
+
+#include "app/ModeSwitch.h"
+#include "service/Button.h"
 
  /* CONSTANTS **************************************************************************************/
 
@@ -22,20 +26,34 @@
  /* PROTOTYPES *************************************************************************************/
 
  /* VARIABLES **************************************************************************************/
-
+int gModeStatus = 0;
  /* EXTERNAL FUNCTIONS *****************************************************************************/
 
  /* INTERNAL FUNCTIONS *****************************************************************************/
 
-// Implementation of the ModeSwitch_SwitchToNextParameterSet method
-static void ModeSwitch_SwitchToNextParameterSet(void) {
+void ModeSwitch_process(void) {
 
-    //The Button function must be called up here / Variables Return parameters still need to be added
+do 
+{
 
-    printf("Switching to the next parameter set...\n");
-}
+    if (3 == Button_getState(0))
+    {
+        gModeStatus = 1;
+    }
+   
+    
+    if (3 == Button_getState(1))
+    { 
+        gModeStatus = 2;
+    }
 
-// Create and initialize ModeSwitch_Handler instance
-ModeSwitch_Handler ModeSwitch_Handler_instance = {
-    .ModeSwitch_SwitchToNextParameterSet = ModeSwitch_SwitchToNextParameterSet
+
+    if (3 == Button_getState(2))
+    {
+        gModeStatus = 3;
+    }
+
+} while (0 == gModeStatus);
+   
+    return;
 };
