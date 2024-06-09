5c649f272e66fdca665ab95b706f686fc6c352c9
GRL2490 <163773856+GRL2490@users.noreply.github.com>
2024-06-09 18:33:39 +0200

    Update ModeSwitch_Handler.c

--- Beistellung_Implementierung/system/Coding/src/app/ModeSwitch_Handler.c
+++ Beistellung_Implementierung/system/Coding/src/app/ModeSwitch_Handler.c
@@ -13,9 +13,16 @@
 
  /* INCLUDES ***************************************************************************************/
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "app/ModeSwitch.h"
+#include "app/StateHandler.h"
 #include "service/Button.h"
+#include "service/Buzzer.h"
+#include "service/Display.h"
+
+
 
  /* CONSTANTS **************************************************************************************/
 
@@ -23,37 +30,107 @@
 
  /* TYPES ******************************************************************************************/
 
+typedef enum tag_ModeStatus
+{
+    NO_SELECTION = 0,    /**< No Selection   */
+    HIGHSPEED_MODE,      /**< Highspeed Mode */
+    SAFETY_MODE,         /**< Safety Mode    */
+    BALANCED_MODE,       /**< Balanced Mode  */
+
+} ModeStatus;
+
+#define MODESWITCH_TEXTLENGTH 30
+#define ONE_SECOND 1
+
  /* PROTOTYPES *************************************************************************************/
 
  /* VARIABLES **************************************************************************************/
-int gModeStatus = 0;
+
+ int gCounterVariableButton = 0;
+
  /* EXTERNAL FUNCTIONS *****************************************************************************/
 
  /* INTERNAL FUNCTIONS *****************************************************************************/
 
-void ModeSwitch_process(void) {
+Events ModeSwitch_process(void) {
 
-do 
-{
-
-    if (3 == Button_getState(0))
-    {
-        gModeStatus = 1;
-    }
-   
+    Display_clear();
+    Display_write("Switch the Mode with Button B",MODESWITCH_TEXTLENGTH);
     
-    if (3 == Button_getState(1))
-    { 
-        gModeStatus = 2;
+
+    sleep(ONE_SECOND);
+    
+
+    Display_write("You have 5 Sec after the Beep",MODESWITCH_TEXTLENGTH);
+    
+
+    Buzzer_beep(BUZZER_NOTIFY);
+
+
+    time_t start_time, current_time;
+    double elapsed_time;
+
+    start_time = time(NULL); // Startzeit speichern
+
+    while (TRUE) {
+        current_time = time(NULL);
+        elapsed_time = difftime(current_time, start_time);  /**< Determination of elapsed time */
+
+        if (BUTTON_STATE_TRIGGERED == Button_getState(BUTTON_ID_B))
+        {
+
+        gCounterVariableButton ++;
+        
+        }
+
+        if (elapsed_time >= 5)   /**< If 5 seconds have passed, end the loop */
+        {                            
+            break;
+        }
+      
+        sleep(ONE_SECOND);       /**<Wait a second */ 
     }
 
 
-    if (3 == Button_getState(2))
+switch (gCounterVariableButton)
     {
-        gModeStatus = 3;
+    case HIGHSPEED_MODE:
+        Display_write("Highspeed Mode is selected",MODESWITCH_TEXTLENGTH);
+        
+        GlobalVelocity = 0;              
+        gCounterVariableButton = 0;
+        break;
+
+    case SAFETY_MODE:
+        Display_write("Safety Mode is selected",MODESWITCH_TEXTLENGTH);
+        
+        GlobalVelocity = -200;
+        gCounterVariableButton = 0;
+        break;
+
+    case BALANCED_MODE:
+        Display_write("Balanced Mode is selected",MODESWITCH_TEXTLENGTH);
+
+        GlobalVelocity = -100;
+        gCounterVariableButton = 0;
+        break;
+
+    default:
+        Display_write("No mode is selected",MODESWITCH_TEXTLENGTH);
+        GlobalVelocity = -400;
+        gCounterVariableButton = 0;
+        break;
     }
 
-} while (0 == gModeStatus);
-   
-    return;
+
+    if(-400 == GlobalVelocity)
+    {
+       return EV_MODE_SWITCH_FAILED;
+    }
+    else
+    {
+       return EV_MODE_SWITCH_SUCCESSFUL;
+    }
+    
+
 };
