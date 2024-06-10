a55383bb79c89ac92b9064855c6e51d877687ef7
steffenhettig <145577050+steffenhettig@users.noreply.github.com>
2024-06-10 10:28:11 +0200

    Create Init.h

---
+++ Beistellung_Implementierung/system/Coding/src/app/Init.h
@@ -0,0 +1,41 @@
+
+/***************************************************************************************************
+  (c) NewTec GmbH 2024   -   www.newtec.de
+***************************************************************************************************/
+/**
+ * @file       Init.h
+ *
+ * Module description comes here.
+ */
+/**************************************************************************************************/
+#ifndef INIT_H
+#define INIT_H
+
+/* INCLUDES ***************************************************************************************/
+#include "hal/Gpio.h"
+#include "hal/Hal.h"
+#include "hal/Irq.h"
+#include "hal/Pwm.h"
+#include "hal/TickTimer.h"
+#include "os/ErrorHandler.h"
+#include "os/Os.h"
+#include "os/Scheduler.h"
+#include "os/SoftTimer.h"
+#include "os/Task.h"
+#include "service/Button.h"
+#include "service/Buzzer.h"
+#include "service/Display.h"
+#include "service/DriveControl.h"
+#include "service/Led.h"
+#include "service/LineSensor.h"
+#include "service/Service.h"
+
+/* CONSTANTS **************************************************************************************/
+
+/* MACROS *****************************************************************************************/
+
+/* TYPES ******************************************************************************************/
+
+/* PROTOTYPES *************************************************************************************/
+
+#endif /* INIT_H */
